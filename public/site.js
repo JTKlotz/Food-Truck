4904c6d5b62647727705158818f969192159e6f3
Klotz, Jordan <800076254@fvtc.edu>
2025-12-01 19:52:00 -0600

    created and tested a showmenu and showEvent function that will eventually populate the index

---
+++ public/site.js
@@ -0,0 +1,54 @@
+const menuList = document.querySelector(".menuList")
+const eventsList = document.querySelector(".eventsList")
+
+
+const menu = [
+    {id: 1, name: 'item1', image: '', details: 'Details about item 1', price: '$15.00'},
+    {id: 2, name: 'item2', image: '', details: 'Details about item 2', price: '$12.00'}
+    
+]
+
+const events = [
+    {id: 1, name: 'event1', image: '', details: 'Details about event 1', time: '7/11/2025'},
+    {id: 2, name: 'event2', image: '', details: 'Details about event 2', time: '7/11/2025'}
+    
+]
+
+
+const showMenu = menu => {
+	menu?.forEach(({id, name, image, details, price}) => {
+		const menuItem = document.createElement("div")
+		menuItem.className = "menu-item"
+		menuItem.innerHTML = `
+			<img src="${image}" alt="${name}">
+			<h2>${name}</h2>
+			<p><strong>Details</strong> ${details}</p>
+            <p><strong>Price:</strong> ${price}</p>
+            <br/>
+		`
+		//recipeItem.onclick = () => showRecipeDetails(id)
+		menuList.appendChild(menuItem)
+	})
+}
+
+const showEvent = events => {
+	events?.forEach(({id, name, image, details, time}) => {
+		const eventItem = document.createElement("div")
+		eventItem.className = "event-list"
+		eventItem.innerHTML = `
+			<img src="${image}" alt="${name}">
+			<h2>${name}</h2>
+			<p><strong>Details</strong> ${details}</p>
+            <p><strong>Date and Time</strong> ${time}</p>
+            <br/>
+		`
+		//eventItem.onclick = () => showEventDetails(id)
+		eventsList.appendChild(eventItem)
+	})
+}
+
+
+
+showMenu(menu)
+showEvent(events)
+
---
+++ routes/static.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const router = require('express').Router()
+
+
+const root = path.join(__dirname, '..', 'public') 
+
+router.get('/', (request, response) => {
+    response.sendFile('index.html', {root})
+})
+
+
+module.exports = router
